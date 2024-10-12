ad2fddf65856fd7cb9f9abfd9841500d6404f7de
Erick Daniel <erick.olano@gmail.com>
2024-10-12 01:53:22 -0500

    Estoy en el video 123 - Countries Service - Restcountries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+// import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,10 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // HttpClientModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
