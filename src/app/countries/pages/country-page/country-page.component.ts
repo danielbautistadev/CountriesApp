33e5c9cf396c98d8d613d762f7d113b40087ae6c
Erick Daniel <erick.olano@gmail.com>
2024-10-14 02:50:02 -0500

    Estoy viendo la seccion nro. 129 - SwitchMap - Refactorizacion

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -12,21 +13,46 @@ export class CountryPageComponent implements OnInit {
 
   constructor( 
     private activatedRoute: ActivatedRoute,
-    private countriesService: CountriesService ){ }
+    private countriesService: CountriesService,
+    private router: Router ){ }
 
   ngOnInit(): void {
 
     this.activatedRoute.params
-      .subscribe ( ({ id }) => {
-       
-        this.countriesService.searchCountryByAlphaCode( id )
-          .subscribe( country => {
-            console.log( { country } );
-          });
+      // .subscribe ( ({ id }) => {
+      //   this.searchCountry(id);
+      // });
+      // .subscribe( this.searchCountry );
 
+      .pipe(
+        // tap( console.log )
+        switchMap( ({id}) => this.countriesService.searchCountryByAlphaCode( id ) )
+      )
+      .subscribe( country => {
+        
+        // console.log( {country} );
+        if( !country ) {
+          return this.router.navigateByUrl('');
+        }
+
+        console.log('Tenemos un país!!');
+        return;
       });
     
   }
+
+  // searchCountry( code: string) {
+  //   this.countriesService.searchCountryByAlphaCode( code )
+  //         .subscribe( country => {
+  //           console.log( { country } );
+  //         });
+  // }
+  // searchCountry( params: Params) {
+  //   this.countriesService.searchCountryByAlphaCode( params['id'] )
+  //     .subscribe( country => {
+  //       console.log( { country } );
+  //     });
+  // }
 
   
 
