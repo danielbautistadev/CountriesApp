a85a76f7c3b187120c306f8ad585364415656890
Erick Daniel <erick.olano@gmail.com>
2024-10-17 00:16:22 -0500

    video nro. 138 - Mostrar y ocultar el componente LoadingSpinner

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -10,18 +10,21 @@ import { Country } from '../../interfaces/country';
 export class ByCapitalPageComponent {
 
   public countries: Country[] = [];
+  public isLoading: boolean = false; // 1. Inicializamos un propiedad booleana en false para mostrar el spinner cuando se realiza la petición HTTP
 
   constructor( private countriesServices: CountriesService ) {
 
   }
 
   searchByCapital( term: string ): void {
-    // console.log('Desde ByCapitalPage');
+
+    this.isLoading = true; // 2. Cuando se realiza la petición HTTP, cambiamos el valor de la propiedad a true para mostrar el spinner
     this.countriesServices.searchCapital(term)
       .subscribe( countries => {
         this.countries = countries;
+        this.isLoading = false; // 3. Cuando se recibe la respuesta de la petición HTTP, cambiamos el valor de la propiedad a false para ocultar el spinner
       });
-    // console.log({term});
+
   }
 
 }
